5f5b7c02c7e35d442d4987b70b4b53dd51a24566
wearrier <a.wearrier@live.jp>
2026-04-23 13:51:21 +0900

    Initial Commit

---
+++ testGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testGame
+//
+//  Created by wearrier on 2026/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testGame/testGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGameApp.swift
+//  testGame
+//
+//  Created by wearrier on 2026/04/23.
+//
+
+import SwiftUI
+
+@main
+struct testGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
